4666eb83b956e1c60e5913e5f7807a2d55c92ab2
scgallagher <seangallagher135@gmail.com>
2020-01-05 13:49:19 -0500

    Initialized spring boot project

---
+++ grandmaster/src/main/java/com/scg/grandmaster/GrandmasterApplication.java
@@ -0,0 +1,13 @@
+package com.scg.grandmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrandmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrandmasterApplication.class, args);
+	}
+
+}
---
+++ grandmaster/src/test/java/com/scg/grandmaster/GrandmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scg.grandmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrandmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
