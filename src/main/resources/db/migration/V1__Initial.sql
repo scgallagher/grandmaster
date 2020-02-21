0fb2e309c33d1783799899a7c53b38bc612725b8
scgallagher <seangallagher135@gmail.com>
2020-02-20 21:59:19 -0500

    added flyway

---
+++ src/main/resources/db/migration/V1__Initial.sql
@@ -0,0 +1,4 @@
+CREATE TABLE game_state (
+	id SERIAL NOT NULL PRIMARY KEY,
+	state VARCHAR NOT NULL
+);
---
+++ src/main/resources/db/migration/V2__AlterGameState.sql
@@ -0,0 +1,5 @@
+ALTER TABLE game_state ADD COLUMN created_at TIMESTAMP NOT NULL DEFAULT NOW();
+
+-- Change state column to json
+ALTER TABLE game_state DROP COLUMN state;
+ALTER TABLE game_state ADD COLUMN state JSONB NOT NULL;
